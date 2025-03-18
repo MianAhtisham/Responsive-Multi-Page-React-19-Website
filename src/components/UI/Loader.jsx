9246363504b0d12ad8878a73e729f460b29fda2f
MianAhtisham <meharyounes067@gmail.com>
2025-03-18 20:57:36 +0500

    Initial commit

---
+++ src/components/UI/Loader.jsx
@@ -0,0 +1,8 @@
+import "../../App.css";
+
+export function Loader() {
+  return <div className="loader"></div>;
+    // <div className="container loader-section">
+      
+    {/* </div> */}
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Herosection } from "../components/UI/Herosection";
+import { About } from "./About";
+export const Home = () => {
+  return (
+    <>
+  <Herosection />
+  <About />
+    </>
+  );
+};
